4f1c383e053aa958bd3509faee35306b51cd212e
neilASC4 <cmantle721@gmail.com>
2017-08-02 19:26:08 -0400

    weather

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ $.ajax({
         var loc = data.current_observation.display_location.full
         var temp = data.current_observation.temperature_string
         var condit = data.current_observation.weather
-        console.log(condit)
+        var woto = data.current_observation.icon_url
+        console.log(woto)
       console.log(data.current_observation)
       $('h2').append(loc);
       $('h3').append("Temperature: "+temp);
-      $('h4').append("Condition: "+condit);
+      $('h4').append("Condition: "+condit+" ");
+      $('h4').append('<img src="' + woto + '">')
     }
 })
 
